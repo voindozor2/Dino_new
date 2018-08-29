f34cd749f0087ad86782c9d41ab7e92d3d11f041
VD2 <voindozor10@gmail.com>
2018-08-29 14:35:46 +0300

    Сделал таймер

--- src/Playing_field.java
+++ src/Playing_field.java
@@ -1,16 +1,23 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class Playing_field extends JPanel implements KeyListener {
+public class Playing_field extends JPanel implements KeyListener{
     Dino dino ;
+    Timer time;
+    boolean game_is_start;
     Playing_field(){
     this.addKeyListener(this);
     this.setFocusable(true);
     this.requestFocusInWindow();
     dino = new Dino();
+    time =  new Timer();
+    game_is_start=false;
     }
 
     @Override
@@ -18,15 +25,27 @@ public class Playing_field extends JPanel implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if(e.getKeyCode()==KeyEvent.VK_UP){
-            System.out.println("Отпустило вверх");
-            dino.setY(dino.getY()+100);
-            repaint();
-        }
-        else if(e.getKeyCode()==KeyEvent.VK_DOWN){
-            System.out.println("Отпустило вниз");
-            dino.setY(dino.getY()-100);
-            repaint();
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
+            if(game_is_start==true) {
+                System.out.println("Отпустило вверх");
+                dino.setY(dino.getY() + 100);
+                repaint();
+            }
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            if(game_is_start==true) {
+                System.out.println("Отпустило вниз");
+                dino.setY(dino.getY() - 100);
+                repaint();
+            }
+        } else if (e.getKeyCode()== KeyEvent.VK_SPACE){
+            time.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    System.out.println("Hello!");
+                    return;
+                }
+            },100,100);
+            game_is_start=true;
         }
 
     }
